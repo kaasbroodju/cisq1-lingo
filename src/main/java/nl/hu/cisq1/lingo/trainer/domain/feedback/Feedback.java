d7cc63bc9fa0dabb64333535b1f63979c20b6dd2
kaasbroodju <morriswaaijer8@gmail.com>
2021-03-22 00:44:57 +0100

    simple green

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/feedback/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/feedback/Feedback.java
@@ -6,6 +6,24 @@ import java.util.*;
 
 public class Feedback extends ArrayList<FeedbackPart> {
     public static Feedback generateFeedback(Word solution, Word guess) {
-        return null;
+        Feedback output = new Feedback();
+
+        // return invalid list when word length does not fit.
+//        if (solution.getLength() != guess.getLength()) {
+//            for (char c : guess.getValue().toCharArray()) {
+//                output.add(new FeedbackPart(c, Mark.INVALID));
+//            }
+//            return output;
+//        }
+        for (Integer i = 0; i < guess.getLength(); i++) {
+            char solutionChar = solution.getValue().charAt(i);
+            char guessChar = solution.getValue().charAt(i);
+            if (solutionChar == guessChar) {
+                output.add(new FeedbackPart(guessChar, Mark.CORRECT));
+            } else {
+                output.add(new FeedbackPart(guessChar, Mark.INCORRECT));
+            }
+        }
+        return output;
     }
 }
--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/feedback/FeedbackPart.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/feedback/FeedbackPart.java
@@ -1,5 +1,7 @@
 package nl.hu.cisq1.lingo.trainer.domain.feedback;
 
+import java.util.Objects;
+
 public class FeedbackPart {
     private Character letter;
     private Mark mark;
@@ -20,5 +22,18 @@ public class FeedbackPart {
     @Override
     public String toString() {
         return letter + " " + mark.name().toLowerCase();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FeedbackPart that = (FeedbackPart) o;
+        return Objects.equals(letter, that.letter) && mark == that.mark;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(letter, mark);
     }
 }
